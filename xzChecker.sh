4d1db4f0820c9188bd5837066d1514e532408cc9
Melih Aybar <melihaybar2606@gmail.com>
2024-05-31 15:36:54 +0300

    first commit

---
+++ xzChecker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the version of xz
+version=$(xz --version 2>&1 | head -n 1 | awk '{print $NF}')
+
+# Check if the version is 5.6.0 or 5.6.1
+if [[ "$version" == "5.6.0" || "$version" == "5.6.1" ]]; then
+  echo "Your xz version is affected by CVE-2024-3094"
+else
+  echo "Your xz version is not affected"
+fi
